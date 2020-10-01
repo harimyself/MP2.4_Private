69688986dcbb63b25f6584a8398530a98fb83af1
Hari <hbojja@equinix.com>
2020-10-01 08:56:40 -0700

    OkapiBM25(k1=2.2,b=0.95,k3=500)  and 0.356766930635

--- search_eval.py
+++ search_eval.py
@@ -11,7 +11,8 @@ def load_ranker(cfg_file):
     The parameter to this function, cfg_file, is the path to a
     configuration file used to load the index.
     """
-    return metapy.index.DirichletPrior(700)
+
+    return metapy.index.OkapiBM25(k1=2.2,b=0.95,k3=500)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
